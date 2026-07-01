1420ad403e6390980de4c43a263e568421f66ede
Mabel Bernal <mabel.bernal-a@mail.escuelaing.edu.co>
2026-07-01 16:26:22 -0500

    feat: setup inicial del proyecto, configuracion de base de datos y dependencias

---
+++ src/main/java/com/pokepedia/pokecore/PokeCoreSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pokepedia.pokecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeCoreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeCoreSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokepedia/pokecore/PokeCoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokepedia.pokecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeCoreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
