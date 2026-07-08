0e49999490cf24d239ed4087be7f0547480639c7
Mabel Bernal <mabel.bernal-a@mail.escuelaing.edu.co>
2026-07-08 18:00:38 -0500

    agregar EntityGraph a findAll y findById en PokemonJpaRepository - resuelve LazyInitializationException

--- src/main/java/com/pokepedia/pokecore/persistence/repository/relational/PokemonJpaRepository.java
+++ src/main/java/com/pokepedia/pokecore/persistence/repository/relational/PokemonJpaRepository.java
@@ -1,9 +1,13 @@
 package com.pokepedia.pokecore.persistence.repository.relational;
 
 import com.pokepedia.pokecore.persistence.entity.relational.PokemonEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -14,9 +18,13 @@ public interface PokemonJpaRepository extends JpaRepository<PokemonEntity, Long>
 
     Optional<PokemonEntity> findByNationalNumber(Integer nationalNumber);
 
-    @EntityGraph(attributePaths = {"types", "stats", "region"})
     @Override
+    @EntityGraph(attributePaths = {"types", "stats", "region"})
     Optional<PokemonEntity> findById(Long id);
+
+    @EntityGraph(attributePaths = {"types", "stats", "region"})
+    @Query("SELECT p FROM PokemonEntity p")
+    Page<PokemonEntity> findAll(Pageable pageable);
 
     @EntityGraph(attributePaths = {"types", "region"})
     List<PokemonEntity> findByTypes_NameIgnoreCase(String typeName);
